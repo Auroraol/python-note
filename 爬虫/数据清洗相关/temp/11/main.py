dcfbf6d99cbda6171185f3ffc5ecb9373b2658df
Auroraol <1665834268@qq.com>
2024-11-06 22:48:12 +0800

    2024/11/6

---
+++ 爬虫/数据清洗相关/temp/11/main.py
@@ -0,0 +1,104 @@
+# import xlrd
+import json
+from http.cookies import SimpleCookie
+
+import openpyxl
+from pathlib import Path
+# from tqdm import tqdm
+import requests
+import xlrd
+import pandas as pd
+
+def extract_product_ids(file_path):
+    # 打开Excel文件
+    workbook = openpyxl.load_workbook(file_path)
+    sheet = workbook.active  # 获取活动表（默认第一个表）
+
+    # 使用集合来存储商品ID，自动去重
+    product_ids = set()
+
+    # 遍历工作表中的每一行，从第二行开始（假设第一行是表头）
+    for row_idx in range(2, sheet.max_row + 1):  # sheet.max_row 给出总行数
+        # 假设商品 ID 在第一列
+        product_id = sheet.cell(row=row_idx, column=5).value  # 获取第一列的值
+
+        if product_id:  # 确保商品 ID 不是空值
+            print(product_id)
+            product_id = str(product_id).strip().replace(" ", "")
+            product_ids.add(product_id)
+
+    # 如果需要返回列表格式
+    return list(product_ids)
+
+def save_to_json(data, file_path):
+    # 将商品ID列表转换为逗号分隔的字符串
+    data_str = ",".join(data)  # 使用逗号将商品ID连接成一个字符串
+    # 打印调试信息，确保字符串没有换行符
+    print("商品ID字符串:", repr(data_str))  # 使用repr打印字符串，以便查看是否有换行符
+
+    # 将数据保存为JSON格式
+    with open(file_path, 'w') as json_file:
+        # 使用json.dump()时，设置ensure_ascii=False，以确保正确处理非ASCII字符
+        json.dump(data_str, json_file, ensure_ascii=False)
+
+def list_files_in_directory(directory_path):
+    path = Path(directory_path)
+    return [file for file in path.iterdir() if file.is_file()]
+
+if __name__ == '__main__':
+    # directory_path = '26'  # 替换为你的目录路径
+    # output_path = 'josn'
+    #
+    # all_files = list_files_in_directory(directory_path)
+    #
+    # file_paths = all_files  # 文件路径列表
+    #
+    # for file_path in file_paths:
+    #     str_id = file_path.stem  # 获取文件名，不包括扩展名
+    #     json_output_path = f'{output_path}/product_ids_{str_id}.json'
+    #
+    #     unique_product_ids = extract_product_ids(file_path)
+    #     save_to_json(unique_product_ids, json_output_path)
+    #
+    #     print(f"商品ID已保存到 {json_output_path}")
+
+    unique_product_ids = extract_product_ids("test1.xlsx")
+
+    url = 'http://219b3c385a823369.pdd4.myjjing.com/api/admin/goods/v2/list_v2'
+    # rawdata为token
+    rawdata = 'source_data=%7B%22person%22:%22%E5%88%98%E4%B8%B0%E6%B4%81%22%7D; xd-metadata=shop_id=66e29a8543246d8d9e56ad13; sajssdk_2015_new_user_219b3c385a823369_pdd4_myjjing_com=1; sa_jssdk_2015_219b3c385a823369_pdd4_myjjing_com=%7B%22distinct_id%22%3A%22%E7%9C%9F%E7%BB%B4%E6%96%AFJEANSWEST%E7%91%9E%E6%AC%A2%E7%91%B6%E4%B8%93%E5%8D%96%E5%BA%97%3Apdd74578728430%22%2C%22first_id%22%3A%221923b7498741c0-0266f63cd26b2d-4c657b58-1328640-1923b749875197d%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E5%BC%95%E8%8D%90%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fcustomer.xiaoduoai.com%2F%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTkyM2I3NDk4NzQxYzAtMDI2NmY2M2NkMjZiMmQtNGM2NTdiNTgtMTMyODY0MC0xOTIzYjc0OTg3NTE5N2QiLCIkaWRlbnRpdHlfbG9naW5faWQiOiLnnJ%2Fnu7Tmlq9KRUFOU1dFU1TnkZ7mrKLnkbbkuJPljZblupc6cGRkNzQ1Nzg3Mjg0MzAifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22%E7%9C%9F%E7%BB%B4%E6%96%AFJEANSWEST%E7%91%9E%E6%AC%A2%E7%91%B6%E4%B8%93%E5%8D%96%E5%BA%97%3Apdd74578728430%22%7D%7D; isGoodsDetailBackGoodsList=false; SID=2|1:0|10:1727584439|3:SID|44:MjBjMWMxYzQ2ZjNiNGExYThlMWNkZmE1ZmViNzhlMjg=|de5c4035032aaa2d2f61f1584e10a4a2993b5fc68dc8d46b0b4375c380bf12bf'
+    cookie_jar = SimpleCookie()
+    cookie_jar.load(rawdata)
+    cokies = {}
+    for key, morsel in cookie_jar.items():
+        cokies[key] = morsel.value
+
+    # print(goods_ids)
+    param = json.loads('''{
+       "goods_category_id": "",
+       "bound_to_chart": "all",
+       "bound_to_activity": "",
+       "keyword": "162371044481",
+       "keyword_type": "link",
+       "sort": "update_time_desc ",
+       "skip": 0,
+       "limit": 10,
+       "cid": "",
+       "seller_cid": "",
+       "status": 0
+     }''')
+    flag = False  # 标志变量
+    for product_id in unique_product_ids:
+        param["keyword"] = str(product_id)
+        rsp = requests.post(url, json=param, cookies=cokies)
+        j = rsp.json()
+        if not flag:
+            print(j)
+            flag = True
+        if rsp.status_code != 200:
+            print(j)
+            continue
+
+    # save_to_json(unique_product_ids, "product_ids_text.json")
+    #
+    # print(f"商品ID已保存到")
---
+++ 爬虫/数据清洗相关/temp/11/script4.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/9/24 16:37
+# @Author  : IngDao
+# @Email   : 1665834268@qq.com
+# @File    : script.py
+from time import sleep
+
+import requests
+import json
+from http.cookies import SimpleCookie
+
+from main import save_to_json
+
+
+# from tqdm import tqdm
+
+def TEst():
+    ###
+    '''
+    curl "https://4eac46eaaef34fe9.pdd4.myjjing.com/api/admin/goods/v2/list_v2" ^
+      -H "Accept: application/json" ^
+      -H "Accept-Language: zh-CN,zh;q=0.9,en;q=0.8" ^
+      -H "Connection: keep-alive" ^
+      -H "Content-Type: application/json" ^
+      -H "Cookie: gr_user_id=12efe853-5ce6-4395-b9f7-cf6fb0e6e13d; sensorsdata2015jssdkcross=^%^7B^%^22^%^24device_id^%^22^%^3A^%^221761e2c1e5f88b-07b016474e5d41-c791e37-2073600-1761e2c1e6072e^%^22^%^7D; session_id_csm=MTY3MjEzMzM2N3xOd3dBTkZSSk1rczNOamRUTlU5U01rMU5Tak5TUlVwTlJGaERRMU5aV1RSSFdVUllXVTVaVms1WFdEWXlRVnBTUkRZelNsWkhRVkU9fIv-weijTeBmDcAehxzlCL-zKB6cytoyBwDBY1zJqVUg; Hm_lvt_103e9b51f831e7a08a4e57fae4d0fb05=1673253389; sa_jssdk_2015_3f516f31d69f7222_jd4_xiaoduoai_com=^%^7B^%^22distinct_id^%^22^%^3A^%^22^%^E8^%^89^%^BE^%^E6^%^BE^%^8C^%^E5^%^85^%^8B^%^EF^%^BC^%^88^%^E5^%^8D^%^97^%^E4^%^BA^%^AC^%^EF^%^BC^%^89^%^E7^%^94^%^9F^%^E7^%^89^%^A9^%^E7^%^A7^%^91^%^E6^%^8A^%^80^%^E6^%^9C^%^89^%^E9^%^99^%^90^%^E5^%^85^%^AC^%^E5^%^8F^%^B8^%^3Aasknj^%^22^%^2C^%^22first_id^%^22^%^3A^%^2218595ac91bd6ad-07106f3fd78bb18-26021151-2073600-18595ac91be76b^%^22^%^2C^%^22props^%^22^%^3A^%^7B^%^7D^%^2C^%^22identities^%^22^%^3A^%^22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTg1OTVhYzkxYmQ2YWQtMDcxMDZmM2ZkNzhiYjE4LTI2MDIxMTUxLTIwNzM2MDAtMTg1OTVhYzkxYmU3NmIiLCIkaWRlbnRpdHlfbG9naW5faWQiOiLoib7mvozlhYvvvIjljZfkuqzvvInnlJ^%^2Fniannp5HmioDmnInpmZDlhazlj7g6YXNrbmoifQ^%^3D^%^3D^%^22^%^2C^%^22history_login_id^%^22^%^3A^%^7B^%^22name^%^22^%^3A^%^22^%^24identity_login_id^%^22^%^2C^%^22value^%^22^%^3A^%^22^%^E8^%^89^%^BE^%^E6^%^BE^%^8C^%^E5^%^85^%^8B^%^EF^%^BC^%^88^%^E5^%^8D^%^97^%^E4^%^BA^%^AC^%^EF^%^BC^%^89^%^E7^%^94^%^9F^%^E7^%^89^%^A9^%^E7^%^A7^%^91^%^E6^%^8A^%^80^%^E6^%^9C^%^89^%^E9^%^99^%^90^%^E5^%^85^%^AC^%^E5^%^8F^%^B8^%^3Aasknj^%^22^%^7D^%^7D; isGoodsDetailBackGoodsList=false; source_data=^%^7B^%^22person^%^22:^%^22^%^E5^%^86^%^AF^%^E5^%^8D^%^9A^%^22^%^7D; xd-metadata=shop_id=63aa6a18ba26910015d0553d; sso_token=token:f478eb95cebb42d2ac4bb954ad058d8f; SID=2^|1:0^|10:1673434386^|3:SID^|44:ZjdkMmZhZmM3OTE5NDIxMDhmNDY0YTBkNWZmZDllNTg=^|7bdea4a0ade891a5dc543ca8cd75ec8e4bd96be8fe41a5bd4cf82d1e75309205; Hm_lpvt_103e9b51f831e7a08a4e57fae4d0fb05=1673434387; 934a89823bed3e05_gr_last_sent_sid_with_cs1=fd747257-336a-4669-9bd7-b4bf6a2a633d; 934a89823bed3e05_gr_last_sent_cs1=^%^E8^%^89^%^BE^%^E6^%^BE^%^8C^%^E5^%^85^%^8B^%^EF^%^BC^%^88^%^E5^%^8D^%^97^%^E4^%^BA^%^AC^%^EF^%^BC^%^89^%^E7^%^94^%^9F^%^E7^%^89^%^A9^%^E7^%^A7^%^91^%^E6^%^8A^%^80^%^E6^%^9C^%^89^%^E9^%^99^%^90^%^E5^%^85^%^AC^%^E5^%^8F^%^B8^%^3Aasknj; 934a89823bed3e05_gr_cs1=^%^E8^%^89^%^BE^%^E6^%^BE^%^8C^%^E5^%^85^%^8B^%^EF^%^BC^%^88^%^E5^%^8D^%^97^%^E4^%^BA^%^AC^%^EF^%^BC^%^89^%^E7^%^94^%^9F^%^E7^%^89^%^A9^%^E7^%^A7^%^91^%^E6^%^8A^%^80^%^E6^%^9C^%^89^%^E9^%^99^%^90^%^E5^%^85^%^AC^%^E5^%^8F^%^B8^%^3Aasknj; 934a89823bed3e05_gr_session_id=fd747257-336a-4669-9bd7-b4bf6a2a633d; 934a89823bed3e05_gr_session_id_fd747257-336a-4669-9bd7-b4bf6a2a633d=true" ^
+      -H "Origin: https://3f516f31d69f7222.jd4.xiaoduoai.com" ^
+      -H "Referer: https://3f516f31d69f7222.jd4.xiaoduoai.com/goods/list" ^
+      -H "Sec-Fetch-Dest: empty" ^
+      -H "Sec-Fetch-Mode: cors" ^
+      -H "Sec-Fetch-Site: same-origin" ^
+      -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" ^
+      -H "X-B3-Spanid: fc6545f4117343c7" ^
+      -H "X-B3-Traceid: fc6545f4117343c70000001673434428" ^
+      -H "XX-Platform: jdzy" ^
+      -H "sec-ch-ua: ^\^"Not?A_Brand^\^";v=^\^"8^\^", ^\^"Chromium^\^";v=^\^"108^\^", ^\^"Google Chrome^\^";v=^\^"108^\^"" ^
+      -H "sec-ch-ua-mobile: ?0" ^
+      -H "sec-ch-ua-platform: ^\^"Windows^\^"" ^
+      --data-raw "^{^\^"goods_category_id^\^":^\^"^\^",^\^"bound_to_chart^\^":^\^"all^\^",^\^"bound_to_activity^\^":^\^"^\^",^\^"keyword^\^":^\^"100003457272^\^",^\^"keyword_type^\^":^\^"plat_goods_id^\^",^\^"sort^\^":^\^"list_time_desc^\^",^\^"skip^\^":0,^\^"limit^\^":10,^\^"cid^\^":^\^"^\^",^\^"seller_cid^\^":^\^"^\^",^\^"status^\^":0^}" ^
+      --compressed
+    '''
+    # c3e19b73c8c95e44.pdd4.myjjing.com 替换为店铺地址
+    url = 'http://2e143caaf0acfea1.pdd4.myjjing.com/api/admin/goods/v2/list_v2'
+    # rawdata为token
+    rawdata = 'source_data=%7B%22person%22:%22%E5%88%98%E4%B8%B0%E6%B4%81%22%7D; xd-metadata=shop_id=66e28f89d0c22ab5195a8b9d; sajssdk_2015_new_user_2e143caaf0acfea1_pdd4_myjjing_com=1; sa_jssdk_2015_2e143caaf0acfea1_pdd4_myjjing_com=%7B%22distinct_id%22%3A%22%E7%9C%9F%E7%BB%B4%E6%96%AFJEANSWEST%E9%80%85%E8%8E%B1%E4%B8%93%E5%8D%96%E5%BA%97%3Apdd77812446064%22%2C%22first_id%22%3A%221923b73d9c5fd8-0dea045abdc525-4c657b58-1328640-1923b73d9c61c2b%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E5%BC%95%E8%8D%90%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fcustomer.xiaoduoai.com%2F%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTkyM2I3M2Q5YzVmZDgtMGRlYTA0NWFiZGM1MjUtNGM2NTdiNTgtMTMyODY0MC0xOTIzYjczZDljNjFjMmIiLCIkaWRlbnRpdHlfbG9naW5faWQiOiLnnJ%2Fnu7Tmlq9KRUFOU1dFU1TpgIXojrHkuJPljZblupc6cGRkNzc4MTI0NDYwNjQifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22%E7%9C%9F%E7%BB%B4%E6%96%AFJEANSWEST%E9%80%85%E8%8E%B1%E4%B8%93%E5%8D%96%E5%BA%97%3Apdd77812446064%22%7D%7D; isGoodsDetailBackGoodsList=false; SID=2|1:0|10:1727584309|3:SID|44:YjUxYzdjNjJhNjQ1NDhjZWI2MzE5MDZhOTEwYTZkYmE=|479c4fe0df8ba398cc9a50da1eb554205563b9fcee15e804a68f65a8870870c4'
+    cookie_jar = SimpleCookie()
+    cookie_jar.load(rawdata)
+    cokies = {}
+    for key, morsel in cookie_jar.items():
+        cokies[key] = morsel.value
+
+    f = open("29/json/product_ids_text3.json", 'r')
+    goods_ids = json.load(f)
+    # print(goods_ids)
+    param = json.loads('''{
+      "goods_category_id": "",
+      "bound_to_chart": "all",
+      "bound_to_activity": "",
+      "keyword": "162371044481",
+      "keyword_type": "link",
+      "sort": "update_time_desc ",
+      "skip": 0,
+      "limit": 10,
+      "cid": "",
+      "seller_cid": "",
+      "status": 0
+    }''')
+    flag = False  # 标志变量
+    # for goods_id in tqdm(goods_ids, desc='Processing'):
+    #     param["keyword"] = str(goods_id)
+    #     rsp = requests.post(url, json=param, cookies=cokies)
+    #     j = rsp.json()
+    #     if not flag:
+    #         print(j)
+    #         flag = True
+    #     if rsp.status_code != 200:
+    #         print(j)
+    #         continue
+
+if __name__ == '__main__':
+    product_ids = set()
+
+    # 读取第一个 JSON 文件
+    with open("product_ids_text1.json", 'r') as f:
+        goods_ids1 = json.load(f)
+        for goods_id1 in goods_ids1:
+            product_ids.add(goods_id1)
+
+    # 读取第二个 JSON 文件
+    with open("product_ids_text2.json", 'r') as f:
+        goods_ids2 = json.load(f)
+        for goods_id1 in goods_ids2:
+            product_ids.add(goods_id1)
+
+    # 将 set 转换为 list，然后写入到新的 JSON 文件
+    with open("product_ids.json", 'w') as json_file:
+        json.dump(list(product_ids), json_file, ensure_ascii=False)
+
+
